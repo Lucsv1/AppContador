623ed9318f14d3b9118a7b0ae6626ff51039463e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-25 19:09:31 -0300

    finalizando projeto

--- appCont/App.js
+++ appCont/App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [numero1, setNumero1] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>numero: {numero1}</Text>
+      <Button title='acrescentar' onPress={()=>{
+        setNumero1(numero1 + 1)
+      }}/>
+      <Button title='tirar' onPress={()=>{
+        setNumero1(numero1 - 1)
+      }}/>
+      
+      
     </View>
   );
 }
